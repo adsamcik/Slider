7d210289b78f2e6824711ce246611d61c5a4364b
adsamcik <adsamcik@users.noreply.github.com>
2017-10-15 19:29:14 +0200

    Add javadoc to EMath

--- slider/src/main/java/com/adsamcik/slider/EMath.java
+++ slider/src/main/java/com/adsamcik/slider/EMath.java
@@ -1,16 +1,38 @@
 package com.adsamcik.slider;
 
 class EMath {
+	/**
+	 * Returns number of decimal places in float.
+	 * Whole numbers can return 1 instead of 0, however there was no reason to change it for current use case.
+	 *
+	 * @param value Number
+	 * @return Number of decimal places
+	 */
 	static int decimalPlaces(float value) {
 		String text = Float.toString(value);
 		return text.length() - text.indexOf('.') - 1;
 	}
 
+	/**
+	 * Round float numbers to number of decimal places.
+	 *
+	 * @param value     Number
+	 * @param precision Decimal places
+	 * @return Rounded number
+	 */
 	static float round(float value, int precision) {
 		int scale = (int) Math.pow(10, precision);
 		return Math.round(value * scale) / ((float) scale);
 	}
 
+	/**
+	 * Limits value to min-max bounds.
+	 *
+	 * @param value Value
+	 * @param min   Minimum
+	 * @param max   Maximum
+	 * @return Limited value
+	 */
 	static float limit(final float value, final float min, final float max) {
 		if (value < min)
 			return min;
@@ -20,10 +42,26 @@ class EMath {
 			return value;
 	}
 
+	/**
+	 * Limits value to max bound.
+	 *
+	 * @param value Value
+	 * @param max   Maximum
+	 * @return Limited value
+	 */
 	static float limitMax(final float value, final float max) {
 		return value > max ? max : value;
 	}
 
+	/**
+	 * Rounds value to step in desired direction.
+	 * This direction is than added to target value.
+	 *
+	 * @param value     Value
+	 * @param direction Direction
+	 * @param step      Step value
+	 * @return Value after rounding
+	 */
 	static int step(final int value, final int direction, final int step) {
 		int target = value + direction;
 
@@ -34,22 +72,26 @@ class EMath {
 		return target * step;
 	}
 
+	/**
+	 * Rounds value to step.
+	 *
+	 * @param value Value
+	 * @param step  Step value
+	 * @return Value after rounding
+	 */
 	static int step(final int value, final int step) {
 		int left = value % step;
 		int roundDown = value - left;
 		return Math.abs(left) >= Math.abs(step) * 0.5f ? roundDown + step : roundDown;
 	}
 
+	/**
+	 * Returns sign for integer numbers
+	 *
+	 * @param value Value
+	 * @return Signum of value
+	 */
 	static int sign(int value) {
-		if (value > 0)
-			return 1;
-		else if (value < 0)
-			return -1;
-		else
-			return 0;
-	}
-
-	static int sign(float value) {
 		if (value > 0)
 			return 1;
 		else if (value < 0)
