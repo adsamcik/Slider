77222395e674311b2501f17e12bdd11b28017c87
adsamcik <adsamcik@users.noreply.github.com>
2017-10-15 17:53:28 +0200

    Reworked Scale implementation

--- slider/src/main/java/com/adsamcik/slider/ScaleFunctions/LinearScale.java
+++ slider/src/main/java/com/adsamcik/slider/ScaleFunctions/LinearScale.java
@@ -2,9 +2,14 @@ package com.adsamcik.slider.ScaleFunctions;
 
 import com.adsamcik.slider.Slider;
 
-public class LinearScale implements Slider.IScale {
-	@Override
-	public float scale(float progress, float min, float max) {
-		return min + progress * (max - min);
+public class LinearScale {
+	private LinearScale() {}
+
+	public static Slider.IScale<Float> getFloatScale() {
+		return (int progress, int maxProgress, Float min, Float max) -> min + (progress / maxProgress) * (max - min);
+	}
+
+	public static Slider.IScale<Integer> getIntegerScale() {
+		return (int progress, int maxProgress, Integer min, Integer max) -> progress + min;
 	}
 }
