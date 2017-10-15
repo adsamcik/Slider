a0d3c9688ef83d1cb2b7fa6180127fed2c3fcc87
adsamcik <adsamcik@users.noreply.github.com>
2017-10-15 18:11:20 +0200

    Add FloatSlider

---
+++ slider/src/androidTest/java/com/adsamcik/slider/FloatSliderInstrumentationTest.java
@@ -0,0 +1,79 @@
+package com.adsamcik.slider;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class FloatSliderInstrumentationTest {
+	private final float DELTA = 0.00001f;
+
+	@Test
+	public void baseTest() throws Exception {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals("com.adsamcik.slider.test", appContext.getPackageName());
+
+		final int MAX = 9;
+		final int MIN = 1;
+		final int PLACES = 0;
+		final int POW = (int) (100 * Math.pow(10, PLACES));
+		final int MAX_SLIDER = (MAX - MIN) * POW;
+
+		FloatSlider slider = new FloatSlider(appContext);
+		slider.setMinValue((float) MIN);
+		slider.setMaxValue((float) MAX);
+		slider.setStep(2f);
+		slider.setProgressValue(5f);
+		assertEquals(MIN, slider.getMinValue(), DELTA);
+		assertEquals(MAX, slider.getMaxValue(), DELTA);
+		assertEquals(MAX_SLIDER, slider.getMax(), DELTA);
+		assertEquals(4 * POW, slider.getProgress(), DELTA);
+		assertEquals(5, slider.getValue(), DELTA);
+	}
+
+	@Test
+	public void moveTest() throws Exception {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals("com.adsamcik.slider.test", appContext.getPackageName());
+
+		FloatSlider slider = new FloatSlider(appContext);
+		slider.setMinValue(0f);
+		slider.setMaxValue(12f);
+		slider.setStep(3f);
+		for (int i = 0; i < 12; i++) {
+			slider.setProgressValue((float) i);
+			assertEquals(Math.round(i / 3.0) * 3, slider.getValue(), DELTA);
+		}
+	}
+
+	@Test
+	public void negativeNumberTest() throws Exception {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals("com.adsamcik.slider.test", appContext.getPackageName());
+
+		FloatSlider slider = new FloatSlider(appContext);
+		slider.setMinValue(-15f);
+		slider.setMaxValue(15f);
+		slider.setStep(3f);
+		for (int i = -15; i <= 15; i++) {
+			slider.setProgressValue((float) i);
+			assertEquals(Math.round(i / 3.0) * 3, slider.getValue(), DELTA);
+		}
+	}
+}
---
+++ slider/src/main/java/com/adsamcik/slider/FloatSlider.java
@@ -0,0 +1,127 @@
+package com.adsamcik.slider;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+
+import com.adsamcik.slider.ScaleFunctions.LinearScale;
+
+import java.security.InvalidParameterException;
+
+import static com.adsamcik.slider.EMath.round;
+import static com.adsamcik.slider.EMath.step;
+
+public class FloatSlider extends Slider<Float> {
+	private float mStep = 1;
+	private float mMin = 0;
+	private float mMax = 10;
+
+	private int mDecimalPlaces = 0;
+
+	public FloatSlider(Context context) {
+		super(context);
+		init();
+	}
+
+	public FloatSlider(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+		setAttrs(context, attrs);
+	}
+
+	public FloatSlider(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+		setAttrs(context, attrs);
+	}
+
+	public FloatSlider(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		init();
+		setAttrs(context, attrs);
+	}
+
+	private void init() {
+		setScale(LinearScale.getFloatScale());
+	}
+
+	private void setAttrs(Context context, AttributeSet attrs) {
+		TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.FloatSlider);
+		mMin = ta.getFloat(R.styleable.FloatSlider_minFloat, Build.VERSION.SDK_INT >= 26 ? getMin() : 0);
+		mMax = ta.getFloat(R.styleable.FloatSlider_maxFloat, getMax());
+		setStep(ta.getFloat(R.styleable.FloatSlider_stepFloat, 1));
+		ta.recycle();
+		updateText();
+	}
+
+	@Override
+	public void setStep(Float step) {
+		mStep = step;
+		setSliderStep((int)(step * getPercentPower()));
+	}
+
+	@Override
+	public void setProgressValue(Float progress) {
+		if (progress > mMax || progress < mMin)
+			throw new IllegalArgumentException("Progress must be larger than " + mMin + " and smaller than " + mMax + " was " + progress);
+
+		setProgress((int) ((progress - mMin) * getPercentPower()));
+	}
+
+	@Override
+	@RequiresApi(24)
+	public void setProgressValue(Float progress, boolean animate) {
+
+	}
+
+	private int getPercentPower() {
+		return 100 * (int) Math.pow(10, mDecimalPlaces);
+	}
+
+	@Override
+	public void setMinValue(Float min) {
+		if (min >= mMax)
+			throw new InvalidParameterException("Min must be smaller than max");
+
+		mMin = min;
+	}
+
+	@Override
+	public void setMaxValue(Float max) {
+		if (max <= mMin)
+			throw new InvalidParameterException("Max must be larger than min");
+
+		mMax = max;
+		float diff = round(mMax - mMin, 5);
+		int m = (int) (diff * getPercentPower());
+		setMax(m);
+		setSliderStep(Math.round(mStep / diff * max));
+	}
+
+	@Override
+	public Float getMinValue() {
+		return mMin;
+	}
+
+	@Override
+	public Float getMaxValue() {
+		return mMax;
+	}
+
+	@Override
+	public Float getStep() {
+		return mStep;
+	}
+
+	@Override
+	public Float getValue() {
+		return round(mScale.scale(getStepProgress(), getMax(), mMin, mMax), mDecimalPlaces);
+	}
+
+	private int getStepProgress() {
+		return step(getProgress(), getSliderStep());
+	}
+
+}
