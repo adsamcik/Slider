249df242a8731da5d6bf96b5787e98644cc1345b
adsamcik <adsamcik@users.noreply.github.com>
2017-10-19 19:00:36 +0200

    Improved assertion exception detection

--- slider/src/androidTest/java/com/adsamcik/slider/EAssert.java
+++ slider/src/androidTest/java/com/adsamcik/slider/EAssert.java
@@ -1,17 +1,26 @@
 package com.adsamcik.slider;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class EAssert {
 	interface ICallback {
 		void run();
 	}
 
+	/**
+	 * Asserts that given methods throws exception.
+	 *
+	 * @param callback       callback
+	 * @param exceptionClass expected class of the exception
+	 */
 	static void assertException(ICallback callback, Class exceptionClass) {
 		try {
 			callback.run();
-			throw new Exception("Exception of type " + exceptionClass.getName() + " expected.");
+			throw new AssertionError("Exception of type " + exceptionClass.getName() + " expected.");
 		} catch (Exception e) {
+			//Tests for assertion exceptions
+			assertNotEquals(AssertionError.class, e.getClass());
 			assertEquals(exceptionClass, e.getClass());
 		}
 	}
