8edf8cf5280a29861a2eee66a77076ff70d1591d
adsamcik <adsamcik@users.noreply.github.com>
2017-11-01 09:11:36 +0100

    Add TextView test, Refactoring

--- slider/src/androidTest/java/com/adsamcik/slider/IntSliderInstrumentationTest.java
+++ slider/src/androidTest/java/com/adsamcik/slider/IntSliderInstrumentationTest.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
 
 import junit.framework.Assert;
 
@@ -20,16 +21,16 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class IntSliderInstrumentationTest {
+	private Context appContext = InstrumentationRegistry.getTargetContext();
+
 	@Test
 	public void baseTest() throws Exception {
-		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getTargetContext();
-
 		IntSlider slider = new IntSlider(appContext);
 		slider.setMinValue(1);
 		slider.setMaxValue(9);
 		slider.setStep(2);
 		slider.setProgressValue(5);
+		slider.setTextView(null, null);
 		assertEquals(1, (long) slider.getMinValue());
 		assertEquals(9, (long) slider.getMaxValue());
 		assertEquals(8, (long) slider.getMax());
@@ -38,10 +39,36 @@ public class IntSliderInstrumentationTest {
 	}
 
 	@Test
-	public void moveTest() throws Exception {
-		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getTargetContext();
+	public void textViewTest() throws Exception {
+		IntSlider slider = new IntSlider(appContext);
+		TextView textView = new TextView(appContext);
 
+		slider.setTextView(textView, value -> "Test " + value);
+		slider.setMinValue(0);
+		slider.setMaxValue(10);
+		slider.setProgressValue(5);
+
+		assertEquals("Test 5", textView.getText());
+
+		slider.setTextView(null, value -> "Test " + value);
+		slider.setProgressValue(4);
+		assertEquals("Test 5", textView.getText());
+		assertEquals(4, (long) slider.getValue());
+
+		slider.setTextView(textView, value -> "Test " + value);
+		slider.setTextView(textView, null);
+		slider.setProgressValue(6);
+		assertEquals("Test 5", textView.getText());
+		assertEquals(6, (long) slider.getValue());
+
+		slider.setTextView(null, null);
+		slider.setProgressValue(8);
+		assertEquals("Test 5", textView.getText());
+		assertEquals(8, (long) slider.getValue());
+	}
+
+	@Test
+	public void moveTest() throws Exception {
 		IntSlider slider = new IntSlider(appContext);
 		slider.setMinValue(0);
 		slider.setMaxValue(12);
@@ -54,9 +81,6 @@ public class IntSliderInstrumentationTest {
 
 	@Test
 	public void negativeNumberTest() throws Exception {
-		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getTargetContext();
-
 		IntSlider slider = new IntSlider(appContext);
 		slider.setMinValue(-15);
 		slider.setMaxValue(15);
@@ -79,12 +103,12 @@ public class IntSliderInstrumentationTest {
 		slider.setItems(integers);
 		for (int i = 0; i < integers.length; i++) {
 			slider.setProgressValue(integers[i]);
-			assertEquals((long)integers[i], (long) slider.getValue());
+			assertEquals((long) integers[i], (long) slider.getValue());
 		}
 
 		for (int i = 0; i < integers.length; i++) {
 			slider.setProgress(i);
-			assertEquals((long)integers[i], (long) slider.getValue());
+			assertEquals((long) integers[i], (long) slider.getValue());
 		}
 
 		EAssert.assertException(() -> slider.setMinValue(-15), RuntimeException.class);
@@ -105,7 +129,7 @@ public class IntSliderInstrumentationTest {
 		slider.setPreferences(preferences, prefName);
 		slider.setProgressValue(4);
 
-		Assert.assertEquals((long)slider.getValue(), (long)preferences.getInt(prefName, Integer.MIN_VALUE));
+		Assert.assertEquals((long) slider.getValue(), (long) preferences.getInt(prefName, Integer.MIN_VALUE));
 
 		preferences.edit().remove(prefName).apply();
 	}
